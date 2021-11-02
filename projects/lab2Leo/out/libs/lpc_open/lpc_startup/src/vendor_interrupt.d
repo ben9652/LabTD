dea163e640b7554cde96092c8b3b1978374ea02e
ben9652 <ben9652@gmail.com>
2021-11-02 16:01:05 -0300

    lab2Leo eliminado y lab2_AT_Windows creado

--- projects/lab2Leo/out/libs/lpc_open/lpc_startup/src/vendor_interrupt.d
+++
@@ -1,2 +0,0 @@
-projects/lab2Leo/out/libs/lpc_open/lpc_startup/src/vendor_interrupt.o: \
- libs/lpc_open/lpc_startup/src/vendor_interrupt.c
--- projects/lab2Leo/out/libs/sys_newlib/src/system.d
+++
@@ -1,2 +0,0 @@
-projects/lab2Leo/out/libs/sys_newlib//src/system.o: \
- libs/sys_newlib//src/system.c
